23658ad2f60ee59485f95b67e1c8e9b1b4e4651c
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2017-04-28 23:18:20 -0400

    package.json, editorconfig, and lint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "comma-dangle":                ["error", "never"],
+        "comma-spacing":               ["error", { "before": false, "after": true }],
+        "comma-style":                 ["error", "last"],
+        "indent":                      ["error", 4],
+        "linebreak-style":             ["error", "windows"],
+        "no-multi-spaces":             ["error"],
+        "no-ternary":                  ["error"],
+        "one-var":                     ["error", "never"],
+        "quotes":                      ["error", "single"],
+        "semi":                        ["error", "always"],
+        "space-before-function-paren": ["error", "always"],
+        "space-in-parens":             ["error", "never"],
+        "space-infix-ops":             ["error"]
+    }
+};
---
+++ index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+
+var fs = require('fs');
+var path = require('path');
+
+function encodeFLIF (params) {
+    console.log(params);
+}
+
+function decodeFLIF (params) {
+    console.log(params);
+}

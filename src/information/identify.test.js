eec3f5e60236921a10fdca21dc71bd41fb7b644c
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2017-09-03 14:10:06 -0400

    Abstract out filepath in identify test for travis

--- src/information/identify.test.js
+++ src/information/identify.test.js
@@ -5,8 +5,8 @@ function test () {
     var testName = 'identify';
     var path = require('path');
     var testData = [
-        { expected: { file: '/mnt/home/owner/GitHub/node-flif/sample/cat.flif',    dimensions: '80x64',   color: '8-bit RGBA', interlace: 'non-interlaced', size: 103    }, arguments: ['"' + path.join('.', 'sample', 'cat.flif') + '"']    },
-        { expected: { file: '/mnt/home/owner/GitHub/node-flif/sample/output.flif', dimensions: '768x512', color: '8-bit RGB',  interlace: 'interlaced',     size: 475578 }, arguments: ['"' + path.join('.', 'sample', 'output.flif') + '"'] }
+        { expected: { file: '/mnt' + path.join(process.cwd(), 'sample', 'cat.flif'),    dimensions: '80x64',   color: '8-bit RGBA', interlace: 'non-interlaced', size: 103    }, arguments: ['"' + path.resolve('.', 'sample', 'cat.flif') + '"']    },
+        { expected: { file: '/mnt' + path.join(process.cwd(), 'sample', 'output.flif'), dimensions: '768x512', color: '8-bit RGB',  interlace: 'interlaced',     size: 475578 }, arguments: ['"' + path.resolve('.', 'sample', 'output.flif') + '"'] }
     ];
 
     runAllTests(testName, 'information', testData);

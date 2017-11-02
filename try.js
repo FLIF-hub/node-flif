c116372e89670333a487c952510f1e6e400882a6
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2017-11-02 15:08:37 -0400

    Update try.js

--- try.js
+++ try.js
@@ -2,6 +2,11 @@
 
 var fs = require('fs');
 
+// Delete old files from previous attempts
+try { fs.unlinkSync('./sample/try-a.png');  } catch (err) {}
+try { fs.unlinkSync('./sample/try-b.flif'); } catch (err) {}
+try { fs.unlinkSync('./sample/try-c.png');  } catch (err) {}
+
 function runCommandSync (args) {
     var exec = require('child_process').execSync;
     var wasmPath = require.resolve('flif-wasm');
@@ -21,18 +26,27 @@ console.log(version);
 
 
 
-// Should convert cat.flif to try.png
-var decode = runCommandSync('-d ./sample/cat.flif ./sample/try.png');
+// Should convert cat.flif to try-a.png
+var decode = runCommandSync('-d ./sample/cat.flif ./sample/try-a.png');
 console.log(decode);
 
 // Log out true/false if file exists
-console.log(fs.existsSync('./sample/try.png'));
+console.log(fs.existsSync('./sample/try-a.png'));
 
 
 
-// Should convert cat.png to try.flif
-var encode = runCommandSync('-e ./sample/cat.png ./sample/try.flif');
+// Should convert cat.png to try-b.flif
+var encode = runCommandSync('-e ./sample/cat.png ./sample/try-b.flif');
 console.log(encode);
 
 // Log out true/false if file exists
-console.log(fs.existsSync('./sample/try.flif'));
+console.log(fs.existsSync('./sample/try-b.flif'));
+
+
+
+// Should convert try-b.flif to try-c.png
+var decodeEncodedTry = runCommandSync('-d ./sample/try-b.flif ./sample/try-c.png');
+console.log(decodeEncodedTry);
+
+// Log out true/false if file exists
+console.log(fs.existsSync('./sample/try-c.png'));

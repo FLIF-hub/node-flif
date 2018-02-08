1deef49c09680e33d977d6c1b5821a2e5d92fabc
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2018-02-08 15:18:01 -0500

    executable path works when NF is a dep

--- src/helpers/executablePath.js
+++ src/helpers/executablePath.js
@@ -8,7 +8,17 @@ function executablePath () {
 
     if (process.platform === 'win32') {
         var path = require('path');
+        var rootPath = '';
+        try {
+            rootPath = require.resolve('node-flif');
+        } catch (err) {
+            // eslint-disable-line no-empty
+        }
+
         executionPath = path.join('.', 'executables', 'win32', 'flif.exe');
+        if (rootPath) {
+            executionPath = path.join(path.dirname(rootPath), executionPath);
+        }
     } else {
         var wasmPath = require.resolve('flif-wasm');
         executionPath = 'node "' + wasmPath + '"';
